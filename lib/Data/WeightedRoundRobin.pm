49235d2df9a8b34e931c5c51b9c4ddcec4f4a2dd
xaicron <xaicron@gmail.com>
2011-07-27 16:50:32 +0900

    initial commit

---
+++ lib/Data/WeightedRoundRobin.pm
@@ -0,0 +1,42 @@
+package Data::WeightedRoundRobin;
+
+use strict;
+use warnings;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Data::WeightedRoundRobin -
+
+=head1 SYNOPSIS
+
+  use Data::WeightedRoundRobin;
+
+=head1 DESCRIPTION
+
+Data::WeightedRoundRobin is
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron {at} cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2011 - xaicron
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
